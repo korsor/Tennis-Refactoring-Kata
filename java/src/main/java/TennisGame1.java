4824687010cfb93c06e2a373fa983c2c7bcd9805
Tapakorn Wankaew <tapakorn.w@kasikornbank.com>
2017-07-26 08:58:35 +0700

    clean Refactoring TennisGame1

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,6 +1,6 @@
 
 public class TennisGame1 implements TennisGame {
-    
+
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
@@ -20,29 +20,23 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore = 0;
-        if (m_score1==m_score2)
-        {
+        if (m_score1 == m_score2) {
             String[] scoreLove = {"Love-All", "Fifteen-All", "Thirty-All", "Deuce"};
-            if (m_score1==m_score2)
-            {   if(m_score1 <= 2 )
+            if (m_score1 == m_score2) {
+                if (m_score1 <= 2)
                     score = scoreLove[m_score1];
                 else
                     score = scoreLove[3];
             }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage "+player1Name;
-            else if (minusResult == -1) score ="Advantage "+player2Name;
-            else if (minusResult >=2 ) score = "Win for "+player1Name;
-            else score ="Win for "+player2Name;
-        }
-        else
-        {
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            int minusResult = m_score1 - m_score2;
+            if (minusResult == 1) score = "Advantage " + player1Name;
+            else if (minusResult == -1) score = "Advantage " + player2Name;
+            else if (minusResult >= 2) score = "Win for " + player1Name;
+            else score = "Win for " + player2Name;
+        } else {
             String[] scorePoint = {"Love", "Fifteen", "Thirty", "Forty"};
-            score = scorePoint[m_score1]+"-"+scorePoint[m_score2];
+            score = scorePoint[m_score1] + "-" + scorePoint[m_score2];
         }
         return score;
     }
